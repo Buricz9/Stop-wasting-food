c12e8ef085741444851d46990a6fbea116a5cea7
Buricz9 <buricz1906@gmail.com>
2024-05-15 19:48:29 +0200

    update od file

--- postDataController.go
+++ postDataController.go
@@ -4,16 +4,9 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
-	"sort"
-	"strings"
 
 	_ "github.com/go-sql-driver/mysql"
 )
-
-type History struct {
-	Ingridients string
-	Number      int
-}
 
 func connectionOfDataBase(login string, password string) *sql.DB {
 	db, err := sql.Open("mysql", ""+login+":"+password+"@tcp(localhost:3306)/recipedatabase")
@@ -35,65 +28,11 @@ func connectionOfDataBase(login string, password string) *sql.DB {
 
 }
 
-func getAllHistory(db *sql.DB) []History {
-	rows, err := db.Query("SELECT historyOfIngredients, historyOfNumber FROM history_of_inputs")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer rows.Close()
-
-	data := []History{}
-	var ing string
-	var num int
-
-	for rows.Next() {
-		err := rows.Scan(&ing, &num)
-		if err != nil {
-			log.Fatal(err)
-		}
-		data = append(data, History{ing, num})
-	}
-
-	return data
-}
-
-func isHistoryExists(db *sql.DB, ingredients string, number int) bool {
-	allHistory := getAllHistory(db)
-
-	areIngredientsEqual := func(ing1, ing2 string) bool {
-		ingSlice1 := strings.Split(ing1, ",")
-		ingSlice2 := strings.Split(ing2, ",")
-		sort.Strings(ingSlice1)
-		sort.Strings(ingSlice2)
-		return strings.Join(ingSlice1, ",") == strings.Join(ingSlice2, ",")
-	}
-
-	for _, history := range allHistory {
-		if areIngredientsEqual(history.Ingridients, ingredients) && history.Number == number {
-			fmt.Println("History already exists in DB")
-			return true
-		}
-	}
-	fmt.Println("History does not exist in DB")
-	return false
-}
-
 func insertDetailsHistory(db *sql.DB, ingredients string, number int, recipe *Recipe, id int) {
-
-	var cal, carb, prot float64
-	for _, nu := range recipe.Results[0].Nutrition.Nutrients {
-		if nu.Name == "Calories" {
-			cal = nu.Amount
-		} else if nu.Name == "Carbohydrates" {
-			carb = nu.Amount
-		} else if nu.Name == "Protein" {
-			prot = nu.Amount
-		}
-	}
 
 	var pk int
 	query := `INSERT INTO recipes (history_id,title,carbs,proteins,calories) VALUES (?,?,?,?,?) RETURNING id`
-	err := db.QueryRow(query, id, recipe.Title, carb, prot, cal).Scan(&pk)
+	err := db.QueryRow(query, id, recipe.Title, recipe.Carbohydrates, recipe.Protein, recipe.Calories).Scan(&pk)
 	if err != nil {
 		fmt.Println("Error inserting history into DB ----------2")
 		log.Fatal(err)
@@ -120,7 +59,6 @@ func insertDetailsHistory(db *sql.DB, ingredients string, number int, recipe *Re
 }
 
 func insertHistory(db *sql.DB, ingredients string, number int) int {
-	// Insert into history_of_inputs
 	var pk int
 	query := `INSERT INTO history_of_inputs (historyOfIngredients, historyOfNumber) VALUES (?, ?) RETURNING id`
 	err := db.QueryRow(query, ingredients, number).Scan(&pk)
